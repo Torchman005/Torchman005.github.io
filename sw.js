2681d567d9733286785405f2371e08154510cce6
Torchman005 <2318257072@qq.com>
2025-10-07 12:53:10 +0800

    Site updated: 2025-10-07 12:53:09

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759756027150";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759812781307";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
